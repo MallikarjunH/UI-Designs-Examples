319258516aee1c3e60945cb19dd5b7660fc29427
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-06-20 18:13:36 +0530

    v1.2

---
+++ Body Fat Calculator/Body Fat Calculator/TableViewCells.swift
@@ -0,0 +1,54 @@
+//
+//  TableViewCells.swift
+//  Body Fat Calculator
+//
+//  Created by mallikarjun on 20/06/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell1: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+
+class TableViewCell2: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
+
+class TableViewCell3: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
--- Body Fat Calculator/Body Fat Calculator/ViewController.swift
+++ Body Fat Calculator/Body Fat Calculator/ViewController.swift
@@ -8,12 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
 
+    @IBOutlet weak var mainTablevIew: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+       return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+        if(indexPath.section == 0){
+            
+            let cell : TableViewCell1 = tableView.dequeueReusableCell(withIdentifier: "TableViewCell1", for: indexPath) as! TableViewCell1
+            
+            return cell
+        }
+        else if(indexPath.section == 1){
+            
+            let cell : TableViewCell2 = tableView.dequeueReusableCell(withIdentifier: "TableViewCell2", for: indexPath) as! TableViewCell2
+            
+            return cell
+        }
+        else{
+            
+            let cell : TableViewCell3 = tableView.dequeueReusableCell(withIdentifier: "TableViewCell3", for: indexPath) as! TableViewCell3
+            
+            return cell
+            
+        }
+    }
+    
 
 
 }
