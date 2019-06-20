309eb07ea340f2c33d97e6106c62a31e02346cfb
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-06-20 11:37:38 +0530

    v1.0
    Added basic project files

---
+++ Body Fat Calculator/Body Fat Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Body Fat Calculator
+//
+//  Created by mallikarjun on 20/06/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
