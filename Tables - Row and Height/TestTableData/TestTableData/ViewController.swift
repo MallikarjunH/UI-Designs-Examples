154b252a09d80a2f3add6cdc83f4976ff50be932
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-06-21 16:08:45 +0530

    Tables - Rows & Colums
    Added example of Table with Rows and Columns - Like an excel sheet

---
+++ Tables - Row and Height/TestTableData/TestTableData/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TestTableData
+//
+//  Created by mallikarjun on 19/06/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
