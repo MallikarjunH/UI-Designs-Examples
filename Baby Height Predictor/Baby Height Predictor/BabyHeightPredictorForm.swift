4eb107a014ed32a74a837fb998143e81c5b89386
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-06-25 17:27:03 +0530

    BHP - Image Assets Added

---
+++ Baby Height Predictor/Baby Height Predictor/BabyHeightPredictorForm.swift
@@ -0,0 +1,30 @@
+//
+//  BabyHeightPredictorForm.swift
+//  Baby Height Predictor
+//
+//  Created by mallikarjun on 25/06/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class BabyHeightPredictorForm: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Baby Height Predictor/Baby Height Predictor/BabyHeightPredictorResultCells.swift
@@ -0,0 +1,24 @@
+//
+//  BabyHeightPredictorResultCells.swift
+//  Baby Height Predictor
+//
+//  Created by mallikarjun on 25/06/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class BabyHeightPredictorResultCells: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
