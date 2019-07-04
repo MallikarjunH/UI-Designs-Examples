a68bba7254e830c6c35ec605133cf153a6956a7e
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-07-04 08:59:53 +0530

    Baby Bump Tracker - Screen 2 Added

---
+++ Baby Bumb Tracker/Baby Bumb Tracker/Screen2Example/CollectionViewCell2.swift
@@ -0,0 +1,26 @@
+//
+//  CollectionViewCell2.swift
+//  Baby Bumb Tracker
+//
+//  Created by mallikarjun on 04/07/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell2: UICollectionViewCell {
+    
+    @IBOutlet weak var demoImage: UIImageView!
+    @IBOutlet weak var mainImage: UIImageView!
+    
+    @IBOutlet weak var mainBGVIew: UIView!
+    
+    @IBOutlet weak var monthLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        mainBGVIew.layer.cornerRadius = 2
+        
+    }
+}
---
+++ Baby Bumb Tracker/Baby Bumb Tracker/Screen2Example/ViewController2.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController2.swift
+//  Baby Bumb Tracker
+//
+//  Created by mallikarjun on 04/07/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController,UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+     var monthArray = ["1st Month","2nd Month","3rd Month","4th Month","5th Month","6th Month","7th Month","8th Month","9th Month"]
+    
+    @IBOutlet weak var mainCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return monthArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell2Id", for: indexPath) as! CollectionViewCell2
+        
+        cell.demoImage.isHidden = true
+        cell.monthLabel.text = monthArray[indexPath.item]
+        cell.monthLabel.textColor = UIColor.white
+        return cell
+    }
+
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        var cellSize: CGSize
+        
+        let screenRect = UIScreen.main.bounds
+        let screenWidth = screenRect.size.width - 40
+        
+        cellSize = CGSize(width: screenWidth / 2.0, height: 235)
+        
+        return cellSize
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return UIEdgeInsets(top: 10, left: 13, bottom: 10, right: 13)
+    }
+    
+
+
+}
