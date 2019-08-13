8909c5d76a36c7f1d2257621966cd68fd88d5305
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-08-13 14:16:40 +0530

    Health Blogs Project Added

---
+++ HealthBlogs_Web/HealthBlogs_Web/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  HealthBlogs_Web
+//
+//  Created by mallikarjun on 13/08/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+ 
+    @IBOutlet weak var webViewInstance: WKWebView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+       // webViewInstance.uiDelegate = (self as! WKUIDelegate)
+        
+        loadBlogs()
+    }
+
+    func loadBlogs(){
+        
+        let url = URL (string: "https://vidalhealth.com/blog/")
+        let myRequest = URLRequest(url: url!)
+        webViewInstance.load(myRequest)
+    }
+
+}
+
