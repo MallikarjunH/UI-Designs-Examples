c05e2ebb6fa63a02dc80107fbc0de166906be651
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-09-09 09:22:04 +0530

    Show and Hide Password - Example

---
+++ Show_Hide_Password_ObjC/Show_Hide_Password_ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Show_Hide_Password_ObjC
+//
+//  Created by mallikarjun on 09/09/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Show_Hide_Password_ObjC/Show_Hide_Password_ObjC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Show_Hide_Password_ObjC
+//
+//  Created by mallikarjun on 09/09/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
