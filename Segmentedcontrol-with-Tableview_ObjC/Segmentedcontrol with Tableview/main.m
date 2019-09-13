04aaeec5df1842d609f82e8523704b1d16f3ce9d
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-09-13 09:37:10 +0530

    SegmentedControl + TableView Example
    SegmentedControl + TableView Example added - Objective C Code

---
+++ Segmentedcontrol-with-Tableview_ObjC/Segmentedcontrol with Tableview/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Segmentedcontrol with Tableview
+//
+//  Created by Mallikarjun on 09/05/18.
+//  Copyright © 2018 Mallikarjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
