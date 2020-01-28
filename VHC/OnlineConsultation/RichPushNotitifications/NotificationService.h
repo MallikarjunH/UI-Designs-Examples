34de702a1e95bd64ea45de9895dfcefb3271757d
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2020-01-28 16:28:01 +0530

    Rich Push Notitifications
    Rich Push Notitifications classes added

---
+++ VHC/OnlineConsultation/RichPushNotitifications/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  RichPushNotitifications
+//
+//  Created by mallikarjun on 31/10/19.
+//  Copyright © 2019 Clean Bill of Health Private Limited. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
