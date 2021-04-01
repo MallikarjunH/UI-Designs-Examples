ee0ba4218e989eec2ec81aedbb61a5ac3aee1e88
Mallikarjun Hanagandi <mallikarjun.h1410@gmail.com>
2021-04-01 11:56:46 +0530

    material-showcase
    Example of material-showcase (spotlight) is added - Base project added

---
+++ Material-Showcase-SpotLightExample/Material-Showcase-SpotLightExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Material-Showcase-SpotLightExample
+//
+//  Created by EOO61 on 01/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
